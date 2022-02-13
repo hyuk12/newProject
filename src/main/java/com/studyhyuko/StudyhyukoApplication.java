8f55e09a991d7ae4d12bbd2271e070bf5f8478fb
hyuk12 <hotman7025@naver.com>
2022-02-14 05:00:34 +0900

    new

---
+++ src/main/java/com/studyhyuko/StudyhyukoApplication.java
@@ -0,0 +1,13 @@
+package com.studyhyuko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyhyukoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyhyukoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studyhyuko/StudyhyukoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyhyuko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyhyukoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
